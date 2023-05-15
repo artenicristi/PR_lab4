958c945800ffbca4bd0c271b84c77a7e8395025c
Cristian Arteni <carteni@pentalog.com>
2023-05-15 11:32:04 +0300

    lab4

---
+++ client.py
@@ -0,0 +1,65 @@
+import tkinter
+from socket import AF_INET, socket, SOCK_STREAM
+from threading import Thread
+
+f = open('order', 'r')
+nr = int(f.read())
+f = open('order', 'w')
+f.write(str(nr + 1))
+f.close()
+
+
+def receive():
+    while True:
+        try:
+            msg = client_socket.recv(size).decode("utf8")
+            messages.insert(tkinter.END, msg)
+        except OSError:
+            break
+
+
+def send_message(event=None):
+    msg = my_msg.get()
+    my_msg.set('')
+    client_socket.send(bytes(msg, "utf8"))
+    if msg == '!q':
+        client_socket.close()
+        top.quit()
+
+
+def on_closing(event=None):
+    my_msg.set('!q')
+    send_message()
+
+
+top = tkinter.Tk()
+top.title("Chat nr: " + str(nr))
+
+messages_frame = tkinter.Frame(top)
+my_msg = tkinter.StringVar()
+scrollbar = tkinter.Scrollbar(messages_frame)
+messages = tkinter.Listbox(messages_frame, height=15, width=50, yscrollcommand=scrollbar.set)
+scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+messages.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
+messages.pack()
+messages_frame.pack()
+
+entry_field = tkinter.Entry(top, textvariable=my_msg)
+entry_field.bind("<Return>", send_message)
+entry_field.pack()
+send_button = tkinter.Button(top, text="Send", command=send_message)
+send_button.pack()
+
+top.protocol("WM_DELETE_WINDOW", on_closing)
+
+host = 'localhost'
+port = 9999
+
+size = 512
+addr = (host, port)
+client_socket = socket(AF_INET, SOCK_STREAM)
+client_socket.connect(addr)
+
+receive_thread = Thread(target=receive)
+receive_thread.start()
+tkinter.mainloop()
---
+++ server.py
@@ -0,0 +1,59 @@
+from socket import AF_INET, socket, SOCK_STREAM
+from threading import Thread
+
+
+def accept_connections():
+    while True:
+        client_socket, client_address = server.accept()
+        print('%s:%s is now connected.' % client_address)
+        client_socket.send(bytes('Type your name and press enter to connect to the chat', 'utf8'))
+        addresses[client_socket] = client_address
+        Thread(target=client_communication, args=(client_socket,)).start()
+
+
+def client_communication(client_socket):
+    name = client_socket.recv(size).decode('utf8')
+    welcome = 'Welcome %s! To leave, type !q' % name
+    client_socket.send(bytes(welcome, 'utf8'))
+    msg = '%s has joined the chat!' % name
+    broadcast(bytes(msg, 'utf8'))
+    clients[client_socket] = name
+
+    while True:
+        msg = client_socket.recv(size)
+        if msg != bytes('!q', 'utf8'):
+            broadcast(msg, name + ': ')
+        else:
+            client_socket.send(bytes('!q', 'utf8'))
+            client_socket.close()
+            del clients[client_socket]
+            broadcast(bytes('%s has left the chat.' % name, 'utf8'))
+            break
+
+
+def broadcast(msg, prefix=''):
+    for sock in clients:
+        sock.send(bytes(prefix, 'utf-8') + msg)
+
+
+host = 'localhost'
+port = 9999
+size = 512
+addr = (host, port)
+clients = {}
+addresses = {}
+
+
+server = socket(AF_INET, SOCK_STREAM)
+server.bind(addr)
+
+orderNr = open('order', 'w')
+orderNr.write(str(1))
+orderNr.close()
+
+server.listen(5)
+print('The chat is open')
+handle_connections = Thread(target=accept_connections())
+handle_connections.start()
+handle_connections.join()
+server.close()
